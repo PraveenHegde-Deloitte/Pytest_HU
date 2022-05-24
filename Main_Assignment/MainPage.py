45a763a68d1721e2ba40e0ce6713d933a3f9d642
PraveenHegde-Deloitte <PraveenHegde-Deloitte@users.noreply.github.com>
2022-05-24 20:37:12 +0530

    Main page added

---
+++ Main_Assignment/MainPage.py
@@ -0,0 +1,31 @@
+import Admin
+import Register
+import User
+
+class MainPage:
+
+    def main(self):
+        while True:
+            print("\t\t_________________________________________")
+            print("\t\t\t -------Main Screen-------")
+            print("\t\t_________________________________________")
+            print("\t\t\t1-> Admin")
+            print("\t\t\t2-> RegisterUser")
+            print("\t\t\t3-> Userlogin")
+            print("\t\t\t4-> Exit")
+            choice1 = eval(input("\t\t\tEnter your choice(1/2/3):-"))
+            if choice1 == 1:
+                am = Admin.Admin()
+                am.adminscreen()
+            elif choice1 == 2:
+                r=Register.Register()
+                r.user_register()
+            elif choice1 == 3:
+                u=User.User()
+                u.user_login()
+            elif choice1 == 4:
+                exit()
+            else:
+               print("Invalid input")
+mn=MainPage()
+mn.main()
